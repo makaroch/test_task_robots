f6870631e801230a1fc6f0935be60ead3ecd9994
Alex <da.ek.ned>
2024-12-17 17:40:39 +0300

    task 2 done

--- robots/services/__init__.py
+++ robots/services/__init__.py
@@ -1,2 +1,2 @@
 from .ddo import RobotDDO
-from .utils import validate_data_robot, save_new_robot
+from .utils import validate_data_robot, save_new_robot, create_new_robot_report, get_report_period, create_file_response
