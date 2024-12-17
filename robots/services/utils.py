b16ebb3e6c64de54f34ba12e590807d2ce054de8
Alex <da.ek.ned>
2024-12-17 14:22:31 +0300

    фикс валидации

--- robots/services/utils.py
+++ robots/services/utils.py
@@ -3,20 +3,17 @@ from django.http import QueryDict
 from robots.models import Robot
 from robots.services import RobotDDO
 
-def get_all_robots_model() -> list[str]:
-    return Robot.objects.all().values_list("model", flat=True)
 
 def validate_data_robot(data: QueryDict) -> RobotDDO:
     model = data.get("model", None)
     version = data.get("version", None)
     created = data.get("created", None)
-    if model not in get_all_robots_model():
-        raise ValueError("Model not found")
     return RobotDDO(
         model=model,
         version=version,
         created=created,
     )
+
 
 def save_new_robot(robot: RobotDDO) -> None:
     Robot.objects.create(
